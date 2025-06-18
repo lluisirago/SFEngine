a0602c4d431876cfbd578f50409c8aee86d3cf79
Lluís Irago Montaña <dev.lluisirago@gmail.com>
2025-06-18 18:02:45 +0200

    Fixed CMake build process

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,12 +1,14 @@
 function(enable_compiler_warnings TARGET)
-    if (MSVC)
-        target_compile_options(${TARGET} PRIVATE /W4 /permissive-)
-    else()
-        target_compile_options(${TARGET} PRIVATE
-            -Wall
-            -Wextra
-            -Wpedantic
-            -Wno-unused-parameter
-        )
+    if (TARGET ${TARGET})
+        if (MSVC)
+            target_compile_options(${TARGET} PRIVATE /W4 /permissive-)
+        else()
+            target_compile_options(${TARGET} PRIVATE
+                -Wall
+                -Wextra
+                -Wpedantic
+                -Wno-unused-parameter
+            )
+        endif()
     endif()
 endfunction()
---
+++ engine/src/main.cpp
@@ -0,0 +1,6 @@
+int main() {
+    int a       = 1;
+    int b       = 8;
+    int hola[3] = {8, 3, 4};
+    return 0;
+}
